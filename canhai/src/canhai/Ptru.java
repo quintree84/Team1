60de57ec47fa45471be7dc23719af09d43e2ddf1
XBao02 <truongxuanbao2004@gmail.com>
2024-04-03 13:49:04 +0700

    checkin

---
+++ canhai/src/canhai/Ptru.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ptru;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Ptru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          new canbachai().setVisible(true);// TODO code application logic here
+    }
+    
+}
