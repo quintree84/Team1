82f752f70c950da4d2b5f73d3a11eae0d6b4b9b7
Tiffany ( thuy trang ) <132757353+thuytrang3105@users.noreply.github.com>
2024-04-04 16:27:02 +0700

    Check In code

---
+++ MayTinh_ver2/src/maytinh_ver2/MayTinh_ver2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package maytinh_ver2;
+
+/**
+ *
+ * @author 84353
+ */
+public class MayTinh_ver2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new GiaoDienChinh().setVisible(true);// TODO code application logic here
+    }
+    
+}
